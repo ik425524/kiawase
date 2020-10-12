cf1e01398e88f7805da7b67b624b7b9de3466380
ik425524 <yipingxiaoxi286@gmail.com>
2020-10-12 21:25:43 +0900

    ポートフォリオ作成
    what
    マイページにおいて無限スクロールを実
    why
    jqueryのプラグインを使用

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,3 +1,53 @@
 $(function () {
-  $(".topps__container").slick();
+  $(".topps__container").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".bottoms__container").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
