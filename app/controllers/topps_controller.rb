42d56448316008d62f693e57145b5c35aacad0a6
ik425524 <yipingxiaoxi286@gmail.com>
2020-10-27 10:55:24 +0900

    修正

--- app/controllers/topps_controller.rb
+++ app/controllers/topps_controller.rb
@@ -8,7 +8,6 @@ class ToppsController < ApplicationController
 
   def create
     @topp = Topp.new(topp_params)
-    logger.debug @topp.errors.inspect
     if @topp.save
       redirect_to post_path(current_user),notice: "登録が完了しました"    
     else
