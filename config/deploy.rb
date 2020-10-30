03871673b0383b5f2847c9d55abe97b3573a4842
ik425524 <yipingxiaoxi286@gmail.com>
2020-10-30 11:13:03 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 lock '3.14.1'
 
-set :linked_files, %w{ config/credentials.yml.enc }
-
 set :application, 'kiawase'
 
 set :repo_url,  'git@github.com:ik425524/kiawase.git'
@@ -27,16 +25,5 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
-  desc 'upload credentials.yml.enc'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
+end :finishing, 'deploy:cleanup'
 end
